a105b99ea0a2acfe9800dfa67c0cac561eebe44f
amineohn <amineprojet7@gmail.com>
2022-10-07 15:29:07 +0200

    refactor: some shit

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import multer from "multer";
+import multer, { diskStorage } from "multer";
 import fs from "fs";
 import csv from "csvtojson";
+import { config } from "./utils/config";
 
 const app = express();
-const port = 5000;
 
-const storage = multer.diskStorage({
-  destination: (req: Request, file: Express.Multer.File, cb) => {
-    cb(null, "./src/uploads/");
-    req.body.file = file;
-    console.log(file);
-  },
-  filename: (req: Request, file: Express.Multer.File, cb) => {
-    cb(null, file.originalname);
-    req.body.file = file;
-  },
+const upload = multer({
+  storage: diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb) => {
+      cb(null, "./src/uploads/");
+      req.body.file = file;
+      console.log(file);
+    },
+    filename: (req: Request, file: Express.Multer.File, cb) => {
+      cb(null, file.originalname);
+      req.body.file = file;
+    },
+  }),
 });
-
-const upload = multer({ storage: storage });
 
 app.use(cors());
 
@@ -38,11 +38,11 @@ app.post("/uploads", upload.single("file"), (req: Request, res: Response) => {
       `./src/uploads/upload-${id}.csv`
     );
   }
-  console.log(req.file);
 });
 app.get("/read", upload.single("file"), (req: Request, res: Response) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   const { id } = req.query;
+
   if (req.body.file.mimetype === "application/pdf") {
     fs.readFileSync(`./src/uploads/upload-${id}.pdf`);
     res.status(200).send({ message: "File read successfully" });
@@ -65,4 +65,4 @@ app.get("/convert", (req: Request, res: Response) => {
       );
     });
 });
-app.listen(port, () => console.log(`API listening at ${port}`));
+app.listen(config.port, () => console.log(`API listening at ${config.port}`));
